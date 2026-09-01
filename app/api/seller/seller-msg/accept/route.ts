be99eb5a033fe149f5ee0043be66a11383e0ee1c
oochitminn99-crypto <oochitminn99@gmail.com>
2026-09-01 09:43:49 +0630

    email pass

--- app/api/seller/seller-msg/accept/route.ts
+++ app/api/seller/seller-msg/accept/route.ts
@@ -58,8 +58,8 @@ export async function POST(req: Request) {
     const setPasswordUrl = `${process.env.NEXT_PUBLIC_BASE_URL}/seller/set-password?id=${sellerId}`;
 
     const mailOptions = {
-      // `"Support" <${process.env.EMAIL_USER}>`
-      from: process.env.EMAIL_USER,
+      //
+      from:  `"my-market-25kc Support" <${process.env.EMAIL_USER}>`,
       to: sellerEmail,
       subject: 'Set Your Seller Account Password',
       html: `
